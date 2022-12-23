fbf9c1c1eb0cd5b8010435fc1ec0fbaadadfe466
eentrop <108766052+eentrop@users.noreply.github.com>
2022-12-23 14:58:00 -0700

    problems

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 
-console.log("Your index.js file is loaded correctly!");
-
-$("#enter").on("click", function() {
-    $(".popup-overlay", ".popup-content").addClass("active")
+$("#enter").on("click", function(){
+    $(".popup-overlay", ".popup-content").addClass("active");
   });
   
+  $(".close", ".popup-overlay").on("click", function() {
+    $(".popup-overlay", ".popup-content").removeClass("active");
+  }); 
   
-$(".close", ".popup-overlay").on("click", function() {
-    $(".popup-overlay", ".popup-content").removeClass("active")
-  });
